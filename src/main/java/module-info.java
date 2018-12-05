9f2b354851361d99b35ebd6612e1c7c89c335169
Arpita <ahuddar@uci.edu>
2018-12-04 18:54:41 -0800

    tests run

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module mockito.main {
+    requires net.bytebuddy;
+    requires net.bytebuddy.agent;
+    requires junit;
+    requires hamcrest.core;
+    requires objenesis;
+    requires java.instrument;
+}
